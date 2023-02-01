78e71b637f4fa8a285730b745cfa61ac96481533
faiqahsan786 <fahsan@climateclub.com>
2023-02-01 13:38:17 +0500

    fix: templates directory path issue

--- functions/code/core/util/mail.ts
+++ functions/code/core/util/mail.ts
@@ -1,6 +1,7 @@
-import { renderFile } from "ejs";
+import { render } from "ejs";
 import { formatResponse } from ".";
 import { sendSimpleEmail } from "./mailgun";
+import * as fs from "fs";
 
 export const sendEmail = async (
   templateId: string,
@@ -9,10 +10,14 @@ export const sendEmail = async (
 ) => {
   try {
     console.log("sending email ========>");
-    const htmlData = await renderFile(
-      __dirname + `/../templates/${templateId}/${templateId}.ejs`,
-      templateData
+    const fileContents = fs.readFileSync(
+      `./templates/${templateId}/${templateId}.ejs`,
+      {
+        encoding: "utf-8",
+      }
     );
+
+    const htmlData = await render(fileContents, templateData);
     const response = await sendSimpleEmail(
       emailData.recipient,
       emailData.subject,
